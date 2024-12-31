a3283b20d19b0e0ad372b3f4268dace20545eeb3
debug-ing <mahdi.bl4ck@gmail.com>
2024-12-31 06:59:04 +0330

    Edit shell.nix

--- shell.nix
+++ shell.nix
@@ -4,10 +4,12 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.go_1_23
     pkgs.git
+    pkgs.k6
   ];
 
   shellHook = ''
     go install golang.org/x/lint/golint@latest    
     go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    echo "Welcome Developer!"
   '';
 }
